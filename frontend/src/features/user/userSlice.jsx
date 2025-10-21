10adf257cbdcb7a48bf9c28bf809f8741e98099e
Ak1Ena <179331005+Ak1Ena@users.noreply.github.com>
2025-10-21 22:56:00 +0700

    fixed runable

--- frontend/src/features/user/userSlice.jsx
+++ frontend/src/features/user/userSlice.jsx
@@ -1,66 +1,66 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { login, register } from "../../store/actions/userActions";
+// import { createSlice } from "@reduxjs/toolkit";
+// import { login, register } from "../../store/actions/userActions";
 
-const initialToken = localStorage.getItem('userToken');
-const initialUser = localStorage.getItem('currentUser') 
-    ? JSON.parse(localStorage.getItem('currentUser')) 
-    : null;
+// const initialToken = localStorage.getItem('userToken');
+// const initialUser = localStorage.getItem('currentUser') 
+//     ? JSON.parse(localStorage.getItem('currentUser')) 
+//     : null;
 
-const userSlice = createSlice({
-  name: "user",
-  initialState: {
-    user: initialUser, 
-    token: initialToken, 
-    status: "idle", 
-    error: null,
-    isRegistered: false,
-  },
-  reducers: {
-    logout: (state) => {
-      state.user = null;
-      state.token = null;
-      state.status = "idle";
-      localStorage.removeItem('userToken');
-      localStorage.removeItem('currentUser');
-      state.isRegistered = false;
-    },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(login.pending, (state) => {
-        state.status = "loading";
-        state.error = null;
-      })
-      .addCase(login.fulfilled, (state, action) => {
-        state.user = action.payload.user;
-        state.token = action.payload.token;
-        state.status = "success";
-        localStorage.setItem('userToken', action.payload.token);
-        localStorage.setItem('currentUser', JSON.stringify(action.payload.user));
-      })
-      .addCase(login.rejected, (state, action) => {
-        state.status = "error";
-        state.error = action.payload; 
-        state.user = null;
-        state.token = null;
-      })
+// const userSlice = createSlice({
+//   name: "user",
+//   initialState: {
+//     user: initialUser, 
+//     token: initialToken, 
+//     status: "idle", 
+//     error: null,
+//     isRegistered: false,
+//   },
+//   reducers: {
+//     logout: (state) => {
+//       state.user = null;
+//       state.token = null;
+//       state.status = "idle";
+//       localStorage.removeItem('userToken');
+//       localStorage.removeItem('currentUser');
+//       state.isRegistered = false;
+//     },
+//   },
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(login.pending, (state) => {
+//         state.status = "loading";
+//         state.error = null;
+//       })
+//       .addCase(login.fulfilled, (state, action) => {
+//         state.user = action.payload.user;
+//         state.token = action.payload.token;
+//         state.status = "success";
+//         localStorage.setItem('userToken', action.payload.token);
+//         localStorage.setItem('currentUser', JSON.stringify(action.payload.user));
+//       })
+//       .addCase(login.rejected, (state, action) => {
+//         state.status = "error";
+//         state.error = action.payload; 
+//         state.user = null;
+//         state.token = null;
+//       })
 
-      .addCase(register.pending, (state) => {
-        state.status = "loading";
-        state.error = null;
-        state.isRegistered = false;
-      })
-      .addCase(register.fulfilled, (state) => {
-        state.status = "idle";
-        state.isRegistered = true;
-      })
-      .addCase(register.rejected, (state, action) => {
-        state.status = "error";
-        state.error = action.payload;
-        state.isRegistered = false;
-      });
-  },
-});
+//       .addCase(register.pending, (state) => {
+//         state.status = "loading";
+//         state.error = null;
+//         state.isRegistered = false;
+//       })
+//       .addCase(register.fulfilled, (state) => {
+//         state.status = "idle";
+//         state.isRegistered = true;
+//       })
+//       .addCase(register.rejected, (state, action) => {
+//         state.status = "error";
+//         state.error = action.payload;
+//         state.isRegistered = false;
+//       });
+//   },
+// });
 
-export const { logout } = userSlice.actions;
-export default userSlice.reducer;
+// export const { logout } = userSlice.actions;
+// export default userSlice.reducer;
