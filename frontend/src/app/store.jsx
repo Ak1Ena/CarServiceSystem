142f6c82779a5bbbb9df3fc21f72f5c9b1ad3f96
Ak1Ena <179331005+Ak1Ena@users.noreply.github.com>
2025-10-21 08:13:25 +0700

    add tailwind

--- frontend/src/app/store.jsx
+++ frontend/src/app/store.jsx
@@ -1,16 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit"
-// import carReducer from "../features/car/carSlice.js"
-// import userReducer from "../features/user/userSlice.js"
-// import paymentReducer from "../features/payment/paymentSlice.js"
-// import receiptReducer from "../features/receipt/receiptSlice.js"
-// import reserveReducer from "../features/reserve/reserveSlice.js"
+import carReducer from "../features/car/carSlice.js"
+import userReducer from "../features/user/userSlice.js"
+import paymentReducer from "../features/payment/paymentSlice.js"
+import receiptReducer from "../features/receipt/receiptSlice.js"
+import reserveReducer from "../features/reserve/reserveSlice.js"
 
 export default store = configureStore({
     reducer:{
-        // car:carReducer,
-        // user:userReducer,
-        // payment: paymentReducer,
-        // receipt: receiptReducer,
-        // reserve: reserveReducer,
+        car:carReducer,
+        user:userReducer,
+        payment: paymentReducer,
+        receipt: receiptReducer,
+        reserve: reserveReducer,
     }
 })
