560a0ba3f6c0b97c9ef71bab29068797a44d63c0
Ak1Ena <179331005+Ak1Ena@users.noreply.github.com>
2025-10-21 08:31:23 +0700

    Update Routes

--- frontend/src/app/routes.jsx
+++ frontend/src/app/routes.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Routes } from "react-router-dom"
+import { Routes,Route } from "react-router-dom"
 import CarRoutes from "../features/car/route"
 import PaymentRoutes from "../features/payment/route"
 import ReceiptRoutes from "../features/receipt/route"
@@ -8,11 +8,13 @@ import UserRoutes from "../features/user/route"
 export default function AppRoutes(){
     return (
         <Routes>
-            {CarRoutes}
-            {PaymentRoutes}
-            {ReceiptRoutes}
-            {ReserveRoutes}
-            {UserRoutes}
+            <Route path="/cars/*" element={<CarRoutes />}/>
+            <Route path="/users/*" element={<UserRoutes />}/>
+            <Route path="/payments/*" element={<PaymentRoutes />}/>
+            <Route path="/receipts/*" element={<ReceiptRoutes />}/>
+            <Route path="/reservations/*" element={<ReserveRoutes />}/>
+            <Route path="/" element={<div>TESTING</div>}/>
+            <Route path="*" element={<div>404 - Page Not Found</div>} /> 
         </Routes>
     )
 }
